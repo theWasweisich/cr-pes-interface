c41b87e704c94d013ce27c5106883799fae6dddb
theWasweisich <86789640+theWasweisich@users.noreply.github.com>
2024-01-08 13:10:35 +0100

    Shifts update

--- static/js/shifts.js
+++ static/js/shifts.js
@@ -1,30 +1,84 @@
-function get_time(time_str) {
-    return new Date(time_str);
+function update_time(elem) {
+    var elapsed = elem.querySelector('[data-type="elapsed"]');
+    var in_past = false;
+    var elapsed_str = countdown(elem);
+    if (elapsed_str.startsWith("Vergangen")) {
+        in_past = true;
+        elem.style.backgroundColor = "darkred;";
+    }
+    elapsed.innerHTML = elapsed_str;
 }
-function get_time_since(str) {
-    var current_time = new Date();
-    var start_time = get_time(str);
-    // @ts-expect-error
-    var seconds = (current_time - start_time) / 1000;
-    var minutes = seconds / 60;
-    var hours = minutes / 60;
-    var days = hours / 24;
-    var in_future;
-    if (seconds < 0) {
-        in_future = true;
+function countdown(root_elem) {
+    var time = root_elem.querySelector("[data-type=\"start\"]").innerHTML;
+    // Set the date we're counting down to
+    var countDownDate = new Date(time).getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    var past = false;
+    if (Math.floor((distance % (1000 * 60)) / 1000) < 0) {
+        past = true;
+    }
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.abs(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    var hours = Math.abs(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    var minutes = Math.abs(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    var seconds = Math.abs(Math.floor((distance % (1000 * 60)) / 1000));
+    if (days < 1) {
+        var time_str = "<mark>" + days + "</mark> Tag <mark>" + hours + "</mark> Stunden <mark>" + minutes + "</mark> Minuten und <mark>" + seconds + "</mark> Sekunden ";
     }
     else {
-        in_future = false;
+        var time_str = "<mark>" + days + "</mark> Tage <mark>" + hours + "</mark> Stunden <mark>" + minutes + "</mark> Minuten und <mark>" + seconds + "</mark> Sekunden ";
     }
-    return { "secons": seconds,
-        "minutes": minutes,
-        "hours": hours,
-        "days": days };
+    // Output the result
+    if (past) {
+        time_str = "Vergangen seit: " + time_str;
+    }
+    else {
+        time_str = "Beginnt in: " + time_str;
+    }
+    return time_str;
 }
-function ev_Listener(elem) {
-    var elapsed = elem.querySelector('p[data-fill="elapsed"]');
-    var start_time = elem.querySelector('p[data-fill="start"]');
-    console.log(elapsed);
-    var times = get_time_since(start_time.innerHTML);
-    elapsed.innerHTML = String(times);
+function run_on_startup() {
+    var shift_list = document.getElementById("shift_list");
+    var shift_cards = document.querySelectorAll("div.shift_card");
+    var intervals = [];
+    for (var i = 0; i < shift_cards.length; i++) {
+        var shift_card = shift_cards[i];
+        var x = setInterval(this.update_time, 1000, shift_card);
+    }
 }
+run_on_startup();
+var dialog = document.getElementById("shift_creator");
+function dialog_manager() {
+    var shift_name = document.getElementById("shift_name");
+    var shift_start = document.getElementById("time_start");
+    var duration = document.getElementById("duration");
+    console.group("New Shift");
+    console.log("Shift-Name: ".concat(shift_name.value));
+    console.log("Shift-Start: ".concat(shift_start.value));
+    console.log("Shift-Duration: ".concat(duration.value));
+    console.groupEnd();
+    dialog.close();
+}
+function prepare_dialog() {
+    var opener = document.getElementById("open_creator");
+    var closer = document.getElementById("dialog_close");
+    var submitter = document.getElementById("dialog_create");
+    var duration_range = document.getElementById("duration");
+    opener.addEventListener('click', function () {
+        dialog.showModal();
+    });
+    closer.addEventListener('click', function () {
+        dialog.close();
+    });
+    submitter.addEventListener('click', function () {
+        dialog_manager();
+    });
+    var out = duration_range.parentElement.getElementsByTagName("output")[0];
+    duration_range.addEventListener('input', function () {
+        out.innerText = duration_range.value;
+    });
+}
+prepare_dialog();
--- static/typescript/shifts.ts
+++ static/typescript/shifts.ts
@@ -1,38 +1,106 @@
-function get_time(time_str: string): Date {
-    return new Date(time_str)
-}
 
-function get_time_since(str: string) {
-    var current_time = new Date();
-    var start_time = get_time(str);
 
-    // @ts-expect-error
-    var seconds = (current_time - start_time)/1000;
-    var minutes = seconds / 60;
-    var hours = minutes / 60
-    var days = hours / 24;
-    var in_future: boolean;
-    if (seconds < 0) {
-        in_future = true;
-    } else {
-        in_future = false;
+function update_time(elem: HTMLElement) {
+    var elapsed = elem.querySelector('[data-type="elapsed"]');
+
+    var in_past: boolean = false;
+    var elapsed_str: string = countdown(elem)
+    if (elapsed_str.startsWith("Vergangen")) {
+        in_past = true
+        elem.style.backgroundColor = "darkred;"
     }
 
-    return {"secons": seconds, 
-            "minutes": minutes, 
-            "hours": hours,
-            "days": days};
+    elapsed.innerHTML = elapsed_str
 }
 
-function ev_Listener(elem: HTMLElement) {
-    var elapsed = elem.querySelector('p[data-fill="elapsed"]');
-    var start_time = elem.querySelector('p[data-fill="start"]');
-    console.log(elapsed);
+function countdown(root_elem: HTMLElement): string {
 
-    var times = get_time_since(start_time.innerHTML)
-    elapsed.innerHTML = String(times)
+    var time = root_elem.querySelector("[data-type=\"start\"]").innerHTML
+    // Set the date we're counting down to
+    var countDownDate = new Date(time).getTime();
+
+    // Get today's date and time
+    var now = new Date().getTime();
     
-
-
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    var past: boolean = false
+    if (Math.floor((distance % (1000 * 60)) / 1000) < 0) {
+        past = true
+    }
     
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.abs(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    var hours = Math.abs(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    var minutes = Math.abs(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    var seconds = Math.abs(Math.floor((distance % (1000 * 60)) / 1000));
+
+    if (days < 1) {
+        var time_str = "<mark>" + days + "</mark> Tag <mark>" + hours + "</mark> Stunden <mark>" + minutes + "</mark> Minuten und <mark>" + seconds + "</mark> Sekunden ";
+    } else {
+        var time_str = "<mark>" + days + "</mark> Tage <mark>" + hours + "</mark> Stunden <mark>" + minutes + "</mark> Minuten und <mark>" + seconds + "</mark> Sekunden ";
+    }
+    // Output the result
+    if (past) {
+        time_str = "Vergangen seit: " + time_str
+    } else {
+        time_str = "Beginnt in: " + time_str
+    }
+    return time_str
+} 
+
+function run_on_startup() {
+    var shift_list = document.getElementById("shift_list") as HTMLElement
+    var shift_cards = document.querySelectorAll("div.shift_card") as NodeListOf<HTMLElement>
+
+    var intervals: number[] = [];
+
+    for (let i = 0; i < shift_cards.length; i++) {
+        const shift_card = shift_cards[i] as HTMLElement;
+        
+
+        var x = setInterval(this.update_time, 1000, shift_card)
+    }
 }
+
+run_on_startup()
+
+var dialog = document.getElementById("shift_creator") as HTMLDialogElement
+
+function dialog_manager() {
+    const shift_name = document.getElementById("shift_name") as HTMLInputElement
+    const shift_start = document.getElementById("time_start") as HTMLInputElement
+    const duration = document.getElementById("duration") as HTMLInputElement
+    
+    console.group("New Shift")
+    console.log(`Shift-Name: ${shift_name.value}`)
+    console.log(`Shift-Start: ${shift_start.value}`)
+    console.log(`Shift-Duration: ${duration.value}`)
+    console.groupEnd()
+
+    dialog.close()
+
+}
+
+function prepare_dialog() {
+    const opener = document.getElementById("open_creator") as HTMLButtonElement
+    const closer = document.getElementById("dialog_close") as HTMLButtonElement
+    const submitter = document.getElementById("dialog_create") as HTMLButtonElement
+    const duration_range = document.getElementById("duration") as HTMLInputElement
+    
+    opener.addEventListener('click', function() {
+        dialog.showModal()
+    })
+    closer.addEventListener('click', function() {
+        dialog.close()
+    })
+    submitter.addEventListener('click', function() {
+        dialog_manager();
+    })
+    const out = duration_range.parentElement.getElementsByTagName("output")[0]
+
+    duration_range.addEventListener('input', function() {
+        out.innerText = duration_range.value
+    })
+}
+prepare_dialog()
