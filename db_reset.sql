4d53ace87774130aaff23a65d0cbb72e51e4b971
theWasweisich <86789640+theWasweisich@users.noreply.github.com>
2024-04-29 16:34:08 +0200

    Sorting verry viel

--- db_reset.sql
+++
@@ -1,7 +0,0 @@
--- database: c:\Users\langb\OneDrive\PythonProjekte\Python\flask\crepes_interface\datenbank.db
-
--- Use the ▷ button in the top right corner to run the entire file.
-
-DELETE FROM sales; -- delte everything from the sales table
-
-DELETE FROM sqlite_sequence WHERE name="sales"; -- Resets autoincrement
--- production.bat
+++
@@ -1,1 +0,0 @@
-START waitress-serve --host "0.0.0.0" --port 80 app:app
